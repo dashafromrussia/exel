69ccd5059942107529372fbed52af0543e6d8b2b
dashafromrussia <dariavladimirowna@gmail.com>
2021-07-28 17:27:27 +0300

    Table resize added

--- src/core/dom.js
+++ src/core/dom.js
@@ -15,6 +15,10 @@ return this.$el.outHTML.trim()
  }
  // возвр this, чтобы можно было на нем вызвать метод clear..это назыв чейн
 // $('div').html('<h1>text</h1>').clear()
+get data() {
+return this.$el.dataset // возвращ атрибут дата штмл элемента
+}
+
 clear() {
    return this.html('')
 }
@@ -35,6 +39,33 @@ this.$el.addEventListener(eventType, callback)
 remove(eventType, callback) {
     this.$el.removeEventListener(eventType, callback)
 }
+closest(selector) {
+return $(this.$el.closest(selector)) // closest возвращ штмл эл, оборачив его в инстанс класса дом
+}
+getCoords() {
+    return this.$el.getBoundingClientRect()
+}
+get styles() {
+    return this.$el.style
+}
+
+findAll(selector) { // делаем селектор не по всему документу,а только по тому,на кого вешаем эту ф-цию
+    const mass =[]
+    const data = this.$el.querySelectorAll(selector)
+    data.forEach((el)=>{
+        mass.push($(el))
+    })
+    return mass
+}
+css(styles={}) {
+    Object.keys(styles).forEach((el)=>{
+     this.$el.style[el] = styles[el]
+       })
+       return this
+}
+removeEl(data) {
+    this.$el.removeChild(data.$el)
+}
 }
 
 
@@ -49,3 +80,5 @@ $.create=(tagName, classes='')=>{
     }
     return $(el)
 }
+
+
